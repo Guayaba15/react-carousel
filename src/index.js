aecb0a504744e903a97884fd4ac3297598918860
Guayaba15 <daviddavilabarrios@gmail.com>
2024-03-12 22:18:29 -0600

    Primer commit: Agregar librería a GitHub

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState } from 'react';
+import './Carousel.css';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToNextSlide = () => {
+    const newIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToPrevSlide = () => {
+    const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div className="carousel">
+      <button onClick={goToPrevSlide}>Prev</button>
+      <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} />
+      <button onClick={goToNextSlide}>Next</button>
+    </div>
+  );
+};
+
+export default Carousel;
